70d994d68906ce4993c3b1226623fb2ea62cb5c5
Asaad47 <75583625+Asaad47@users.noreply.github.com>
2025-04-26 14:01:01 +0300

    Implement an improvement on DBG algorithm

--- src/dbg.go
+++ src/dbg.go
@@ -212,7 +212,104 @@ func find_eulerian_path(de_bruijn_graph map[string]Node) []string {
 
 // Doesn't assume the graph passes the degree check. Can be any graph.
 func find_all_eulerian_paths(de_bruijn_graph map[string]Node) [][]string {
-	return [][]string{}
+	// find a starting vertex
+	starting_vertices := []string{}
+	for vertex := range de_bruijn_graph {
+		if de_bruijn_graph[vertex].OutDegree-de_bruijn_graph[vertex].InDegree >= 1 {
+			starting_vertices = append(starting_vertices, vertex)
+		}
+	}
+	if len(starting_vertices) == 0 {
+		// no starting vertex found, pick any vertex
+		for vertex := range de_bruijn_graph {
+			starting_vertices = append(starting_vertices, vertex)
+		}
+	}
+
+	paths := [][]string{}
+	path := []string{}
+	visited := make(map[string]bool)
+
+	for len(starting_vertices) > 0 {
+		starting_vertex := starting_vertices[0]
+		start_in_degree := de_bruijn_graph[starting_vertex].InDegree
+		start_out_degree := de_bruijn_graph[starting_vertex].OutDegree
+
+		stack := []string{starting_vertex}
+
+		for len(stack) > 0 {
+			current_vertex := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			if visited[current_vertex] {
+				// found a cycle
+				// TODO: add the cycle to the paths
+				path = append(path, current_vertex)
+				if len(path) > 1 {
+					paths = append(paths, path)
+				}
+				path = []string{}
+				visited = make(map[string]bool)
+				continue
+			}
+
+			visited[current_vertex] = true
+			path = append(path, current_vertex)
+			next_vertex := ""
+			for edge := range de_bruijn_graph[current_vertex].Edges {
+				next_vertex = edge
+				break
+			}
+
+			if next_vertex == "" {
+				// got stuck, no more edges to follow
+				// TODO: add the path to the paths
+				if len(path) > 1 {
+					paths = append(paths, path)
+				}
+				path = []string{}
+				visited = make(map[string]bool)
+				continue
+			}
+
+			stack = append(stack, next_vertex)
+
+			// remove the edge from the graph
+			de_bruijn_graph[current_vertex].Edges[next_vertex]--
+
+			// Update the vertex properties by creating a new vertex with updated values
+			vertex := de_bruijn_graph[current_vertex]
+			vertex.OutDegree--
+			de_bruijn_graph[current_vertex] = vertex
+
+			// Update the next vertex properties
+			nextVertex := de_bruijn_graph[next_vertex]
+			nextVertex.InDegree--
+			de_bruijn_graph[next_vertex] = nextVertex
+
+			if de_bruijn_graph[current_vertex].Edges[next_vertex] == 0 {
+				delete(de_bruijn_graph[current_vertex].Edges, next_vertex)
+			}
+			if de_bruijn_graph[current_vertex].OutDegree == 0 && de_bruijn_graph[current_vertex].InDegree == 0 {
+				delete(de_bruijn_graph, current_vertex)
+			}
+			if de_bruijn_graph[next_vertex].OutDegree == 0 && de_bruijn_graph[next_vertex].InDegree == 0 {
+				delete(de_bruijn_graph, next_vertex)
+			}
+		}
+
+		starting_vertex = starting_vertices[0]
+		start_in_degree_after := de_bruijn_graph[starting_vertex].InDegree
+		start_out_degree_after := de_bruijn_graph[starting_vertex].OutDegree
+		if start_in_degree_after == start_in_degree && start_out_degree_after == start_out_degree {
+			starting_vertices = starting_vertices[1:]
+		} else {
+			// move the starting vertex to the end of the list
+			starting_vertices = append(starting_vertices[1:], starting_vertices[0])
+		}
+	}
+
+	return paths
 }
 
 func DBGAssembler(fastq_filename string, kmer_length int) string {
@@ -237,22 +334,23 @@ func DBGAssembler(fastq_filename string, kmer_length int) string {
 	}
 
 	de_bruijn_graph := construct_de_bruijn_graph(reads, kmer_length)
-	fmt.Println("DBGAssembler: de_bruijn_graph: ", de_bruijn_graph)
-	if !check_graph_degree(de_bruijn_graph) {
-		fmt.Println("DBGAssembler: graph degree is not valid")
-		return ""
-	}
 
-	path := find_eulerian_path(de_bruijn_graph)
-	if len(path) == 0 {
-		fmt.Println("DBGAssembler: no path found")
-		return ""
-	}
+	paths := find_all_eulerian_paths(de_bruijn_graph)
 
 	// assemble the reads from the path
-	assembled_read := ""
-	for _, vertex := range path {
-		assembled_read += vertex[len(vertex)-1:]
+	longest_assembled_read := ""
+	for _, path := range paths {
+		assembled_read := ""
+		for i, vertex := range path {
+			if i == 0 {
+				assembled_read += vertex
+			} else {
+				assembled_read += vertex[len(vertex)-1:]
+			}
+		}
+		if len(assembled_read) > len(longest_assembled_read) {
+			longest_assembled_read = assembled_read
+		}
 	}
 
 	// save the assembled read to a FASTA file
@@ -263,9 +361,16 @@ func DBGAssembler(fastq_filename string, kmer_length int) string {
 	defer fastaFile.Close()
 
 	fastaFile.WriteString(">DBG_assembled_read\n")
-	fastaFile.WriteString(assembled_read)
+	// write on lines with 60 characters
+	for i := 0; i < len(longest_assembled_read); i += 60 {
+		if i+60 > len(longest_assembled_read) {
+			fastaFile.WriteString(strings.ToUpper(longest_assembled_read[i:]) + "\n")
+		} else {
+			fastaFile.WriteString(strings.ToUpper(longest_assembled_read[i:i+60]) + "\n")
+		}
+	}
 
-	return assembled_read
+	return longest_assembled_read
 }
 
 func main() {
@@ -280,4 +385,48 @@ func main() {
 	}
 
 	DBGAssembler(os.Args[1], kmer_length)
+
+	// // debugging
+	// fastqFile, err := os.Open("../toy_dataset/reads_b.fastq")
+	// if err != nil {
+	// 	log.Fatalf("Failed to open FASTQ file: %v", err)
+	// }
+	// defer fastqFile.Close()
+
+	// scanner := bufio.NewScanner(fastqFile)
+
+	// reads := []string{}
+
+	// for scanner.Scan() {
+	// 	line := scanner.Text()
+	// 	if strings.HasPrefix(line, "@") || strings.HasPrefix(line, "+") || strings.HasPrefix(line, "I") {
+	// 		continue
+	// 	}
+
+	// 	read := strings.ToLower(strings.TrimSpace(line))
+	// 	reads = append(reads, read)
+	// }
+
+	// de_bruijn_graph := construct_de_bruijn_graph(reads, 40)
+	// for k, v := range de_bruijn_graph {
+	// 	fmt.Println(k, v)
+	// }
+
+	// paths := find_all_eulerian_paths(de_bruijn_graph)
+	// longest_path := ""
+	// for _, path := range paths {
+	// 	// combine the path into a single string
+	// 	combined_path := ""
+	// 	for i, vertex := range path {
+	// 		if i == 0 {
+	// 			combined_path += vertex
+	// 		} else {
+	// 			combined_path += vertex[len(vertex)-1:]
+	// 		}
+	// 	}
+	// 	if len(combined_path) > len(longest_path) {
+	// 		longest_path = combined_path
+	// 	}
+	// }
+	// fmt.Println(longest_path)
 }
